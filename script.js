546e578ee0769c75330c401054897d19a35a2f3c
SagarMaddi <sagarmaddi1402@gmail.com>
2024-03-21 20:28:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+gsap.from("#img1",{
+    delay:0.4,
+    opacity:0,
+    duration:1.5,
+    y:60
+})
+gsap.from("#img2",{
+    delay:0.4,
+    opacity:0,
+    duration:1.5,
+    x:60
+})
+gsap.from("#img3",{
+    delay:0.4,
+    opacity:0,
+    duration:1.5,
+    y:-60
+})
+gsap.from("#main h1",{
+    delay:0.4,
+    opacity:0,
+    duration:1,
+})
